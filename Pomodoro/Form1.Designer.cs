d8e7a6877c63c59a12c0d3a0ca796fa36ab0be94
gilang <gilang@dev.netkromsolution.com>
2023-03-13 19:36:56 +0700

    first commit

---
+++ Pomodoro/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Pomodoro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonPomodoro = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(793, 445);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.IndianRed;
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.buttonPomodoro);
+            this.tabPage1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(785, 419);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Pomodoro";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(211, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(353, 111);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "25 : 00";
+            // 
+            // buttonPomodoro
+            // 
+            this.buttonPomodoro.BackColor = System.Drawing.SystemColors.Window;
+            this.buttonPomodoro.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonPomodoro.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPomodoro.ForeColor = System.Drawing.Color.IndianRed;
+            this.buttonPomodoro.Location = new System.Drawing.Point(309, 271);
+            this.buttonPomodoro.Name = "buttonPomodoro";
+            this.buttonPomodoro.Size = new System.Drawing.Size(149, 51);
+            this.buttonPomodoro.TabIndex = 0;
+            this.buttonPomodoro.Text = "START";
+            this.buttonPomodoro.UseMnemonic = false;
+            this.buttonPomodoro.UseVisualStyleBackColor = false;
+            this.buttonPomodoro.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.DarkCyan;
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(785, 419);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Short Break";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Window;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Teal;
+            this.button2.Location = new System.Drawing.Point(309, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "START";
+            this.button2.UseMnemonic = false;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(211, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(353, 111);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "05 : 00";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.SteelBlue;
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(785, 419);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Long Break";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Window;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.SteelBlue;
+            this.button3.Location = new System.Drawing.Point(309, 271);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 51);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "START";
+            this.button3.UseMnemonic = false;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(211, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(353, 111);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "15 : 00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Pomodoro Application";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonPomodoro;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Pomodoro/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pomodoro
+{
+    public partial class Form1 : Form
+    {
+        private bool isRunning = false;
+        int second = 25 * 60;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            isRunning = false;
+            switch (tabControl1.SelectedIndex)
+            {
+                case 0:
+                    second = 25 * 60;
+                    break;
+                case 1:
+                    second = 5 * 60;
+                    break;
+                case 2:
+                    second = 15 * 60;
+                    break;
+            }
+            buttonPomodoro.Text = "START";
+            label1.Text = "25 : 00";
+
+            button2.Text = "START";
+            label2.Text = "05 : 00";
+
+            button3.Text = "START";
+            label3.Text = "15 : 00";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isRunning)
+            {
+                buttonPomodoro.Text = "START";
+                isRunning = false;
+                timer1.Enabled = false;
+            }
+            else
+            {
+                buttonPomodoro.Text = "PAUSE";
+                isRunning = true;
+                timer1.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (isRunning)
+            {
+                button2.Text = "START";
+                isRunning = false;
+                timer1.Enabled = false;
+            }
+            else
+            {
+                button2.Text = "PAUSE";
+                isRunning = true;
+                timer1.Enabled = true;
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (isRunning)
+            {
+                button3.Text = "START";
+                isRunning = false;
+                timer1.Enabled = false;
+            }
+            else
+            {
+                button3.Text = "PAUSE";
+                isRunning = true;
+                timer1.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            second = second - 1;
+            if (second < 1)
+            {
+                isRunning = false;
+                timer1.Enabled = false;
+                switch (tabControl1.SelectedIndex)
+                {
+                    case 0:
+                        buttonPomodoro.Text = "START";
+                        label1.Text = "25 : 00";
+                    break;
+                    case 1:
+                        button2.Text = "START";
+                        label2.Text = "05 : 00";
+                    break;
+                    case 2:
+                        button3.Text = "START";
+                        label3.Text = "15 : 00";
+                    break;
+                }
+            }
+            int remainingMinutes = second / 60;
+            int remainingSeconds = second - (remainingMinutes * 60);
+            String minutes = remainingMinutes.ToString("D2") +" : "+remainingSeconds.ToString("D2");
+            switch (tabControl1.SelectedIndex)
+            {
+                case 0:
+                    label1.Text = minutes;
+                    break;
+                case 1:
+                    label2.Text = minutes;
+                    break;
+                case 2:
+                    label3.Text = minutes;
+                    break;
+            }
+        }
+    }
+}
